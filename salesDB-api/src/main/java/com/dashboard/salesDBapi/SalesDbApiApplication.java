4b5c39972837d39889de3491ff4b1300306b1efc
Trevor Hein <treheinguy@gmail.com>
2021-04-17 15:54:33 -0500

    init project setup

---
+++ salesDB-api/src/main/java/com/dashboard/salesDBapi/SalesDbApiApplication.java
@@ -0,0 +1,13 @@
+package com.dashboard.salesDBapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesDbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesDbApiApplication.class, args);
+	}
+
+}
---
+++ salesDB-api/src/test/java/com/dashboard/salesDBapi/SalesDbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard.salesDBapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesDbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
